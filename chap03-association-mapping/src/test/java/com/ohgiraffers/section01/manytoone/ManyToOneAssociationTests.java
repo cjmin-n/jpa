7077d93b6584e64fbea2b94286fb8a352c06d845
jeongmin <mymemose@naver.com>
2024-11-06 11:28:00 +0900

    chap03

---
+++ chap03-association-mapping/src/test/java/com/ohgiraffers/section01/manytoone/ManyToOneAssociationTests.java
@@ -0,0 +1,137 @@
+package com.ohgiraffers.section01.manytoone;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Persistence;
+import org.junit.jupiter.api.*;
+
+public class ManyToOneAssociationTests {
+
+    /*
+    * Association Mapping 은 Entity 클래스 간의 관계를 매핑하는 것을 의미한다.
+    * 이를 통해 객체를 이용해 데이터베이스의 테이블 간의 관계를 매핑할 수 있다.
+    *
+    * 다중성에 의한 분류
+    * 연관 관계가 있는 객체 관계에서는 실제로 연관을 가지는 객체의 수에 따라 분류된다.
+    *
+    * - N : 1 연관관계
+    * - 1 : N 연관관계
+    * - 1 : 1 연관관계
+    *
+    * Jpa 에서 연관관계를 잘못 설정하면 성능과 데이터 일관성에 큰 영향을 줄 수 있다.
+    * 각 연관관계 유형에 따라 jpa 가 데이터베이스와 상호작용 하는 방식이 달라지기 때문에
+    * 잘못된 매핑 설정은 예상치 못한 쿼리를 발생 시키거나, 잘못된 접근을 유발할 수 있다.
+    *
+    * 테이블의 연관 관계는 외래 키를 이용하여 양방향 연관 관계의 특징을 가진다.
+    * 참조에 의한 객체의 연관 관계는 단방향이다.
+    * 객체간의 연관관계를 양방향으로 만들고 싶은 경우 반대 쪽에서도 필드를 추가해
+    * 참조를 보관하면 된다.
+    * 하지만 엄밀하게 이는 양방향 관계가 아니라 단방향 관계 2개로 볼 수 있다.
+    *
+    * */
+
+    private static EntityManagerFactory entityManagerFactory;
+    private EntityManager entityManager;
+
+    @BeforeAll
+    public static void initFactory(){
+        entityManagerFactory = Persistence.createEntityManagerFactory("jpatest");
+    }
+
+    @BeforeEach
+    public void initManager(){
+        entityManager = entityManagerFactory.createEntityManager();
+    }
+
+    @AfterAll
+    public static void closeFactory(){
+        entityManagerFactory.close();
+    }
+
+    @AfterEach
+    public void closeManager(){
+        entityManager.close();
+    }
+
+    /*
+    * ManyToOne 은 다수의 엔티티가 하나의 엔티티를 참조하는 상황에서 사용된다.
+    * 예를 들어 하나의 카테고리가 여러 개의 메뉴를 가질 수 있는 상황에서
+    * 메뉴 엔티티가 카테고리를 참조하는 것이다.
+    * 이 때 메뉴 엔티티가 Many, 카테고리 엔티티가 One 이 된다.
+    *
+    * */
+
+    @Test
+    void 다대일_연관관계_조회_테스트() {
+
+        int menuCode = 15;
+
+        MenuAndCategory foundMenu = entityManager.find(MenuAndCategory.class, menuCode);
+
+        Category menuCategory = foundMenu.getCategory();
+
+        Assertions.assertNotNull(menuCategory);
+        System.out.println("menuCategory = " + menuCategory);
+    }
+
+    @Test
+    void 다대일_연관관계_객체지향쿼리_사용_조회_테스트() {
+        
+        String jpql = "SELECT c.categoryName FROM menu_and_category m JOIN m.category c WHERE m.menuCode = 15";
+        // 이미 menu_and_category 에 join 이 되어있기 때문에 on 을 쓰지 않아도 된다.
+        
+        String category = entityManager.createQuery(jpql, String.class).getSingleResult();
+        
+        Assertions.assertNotNull(category);
+        System.out.println("category = " + category);
+    }
+
+    @Test
+    void 다대일_연관관계_객체_삽입_테스트() {
+
+        MenuAndCategory menuAndCategory = new MenuAndCategory();
+        menuAndCategory.setMenuCode(99999);
+        menuAndCategory.setMenuName("양미리구이");
+        menuAndCategory.setMenuPrice(10000);
+
+        Category category = new Category();
+        category.setCategoryCode(33);
+        category.setCategoryName("신규카테고리");
+        category.setRefCategoryCode(null);
+
+        menuAndCategory.setCategory(category);
+        menuAndCategory.setOrderableStatus("Y");
+
+        EntityTransaction entityTransaction = entityManager.getTransaction();
+        entityTransaction.begin();
+        entityManager.persist(menuAndCategory);
+        entityTransaction.commit();
+
+        MenuAndCategory foundMenuAndCategory = entityManager.find(MenuAndCategory.class, 99999);
+        Assertions.assertEquals(99999, foundMenuAndCategory.getMenuCode());
+        Assertions.assertEquals(33, foundMenuAndCategory.getCategory().getCategoryCode());
+
+    }
+
+
+    @Test
+    void 영속성_삭제_테스트() {
+
+        MenuAndCategory menuAndCategory = entityManager.find(MenuAndCategory.class, 99999);
+        EntityTransaction entityTransaction = entityManager.getTransaction();
+        entityTransaction.begin();
+        entityManager.remove(menuAndCategory);
+        entityTransaction.commit();
+
+        MenuAndCategory deleteMenu = entityManager.find(MenuAndCategory.class, 99999);
+        Category foundCategory = entityManager.find(Category.class, 33);
+        System.out.println(deleteMenu);
+        System.out.println(foundCategory);
+    }
+
+
+    @Test
+    void name() {
+    }
+}
---
+++ chap03-association-mapping/src/test/java/com/ohgiraffers/section01/manytoone/MenuAndCategory.java
@@ -0,0 +1,116 @@
+package com.ohgiraffers.section01.manytoone;
+
+import jakarta.persistence.*;
+
+/*
+* @JoinColumn 은 다대일 연관 관계에서 사용되는 어노테이션이다.
+* 다음과 같은 속성들을 사용할 수 있다.
+*
+* - name : 참조하는 테이블의 컬럼명을 지정한다.
+* - referencedColumnName : 참조되는 컬럼명을 지정한다.
+* - nullable : 참조하는 테이블의 컬럼에 null 값을 허용할지 여부를 지정한다.
+* - unique : 참조하는 테이블의 컬럼에 유일성 제약 조건을 추가할지 여부를 지정한다.
+* - insertable : 새로운 엔티티가 저장될 때, 이 참조 컬럼이 sql insert 문에 포함될지 결정한다.
+* - updatable : 엔티티가 업데이트 될 때, 이 참조 컬럼이 update 문에 포함될지 지정한다.
+* - table : 참조하는 테이블의 이름을 지정한다.
+* - foreignKey : 참조하는 테이블에 생성될 외래 키에 대한 추가 정보를 저장한다.
+*
+* @ManyToOne 은 다대일 연관 관계에서 사용되는 어노테이션이다.
+* 다음과 같은 속성을 사용할 수 있다.
+* - cascade : 연관된 엔티티에 대한 영속성 전이를 설정한다.
+* - fetch : 연관된 엔티티를 로딩하는 전략을 설정한다.
+* - optional : 연관된 엔티티가 필수인지 선택적인지 설정한다.
+* */
+
+@Entity(name = "menu_and_category")
+@Table(name = "tbl_menu")
+public class MenuAndCategory {
+
+    @Id
+    @Column(name = "menu_code")
+    private int menuCode;
+
+    @Column(name = "menu_name")
+    private String menuName;
+
+    @Column(name = "menu_price")
+    private int menuPrice;
+
+    /*
+    * CascadeType
+    * - PERSIST : 자식의 값이 저장될 때 연관관계를 가지고 있는 부모의 값도 함께 저장됨.
+    * - REMOVE : 엔티티를 제거할 때 연관된 엔티티도 모두 제거한다. 참조하고 있는 자식이 여러개 있으면 지워지지 않는다.
+    * - MERGE : 엔티티 상태를 병합할 때 연관된 하위 엔티티도 모두 병합한다.
+    * */
+    @JoinColumn(name = "category_code")
+//    @ManyToOne(cascade = CascadeType.PERSIST)
+//    @ManyToOne(cascade = CascadeType.REMOVE)
+    @ManyToOne(cascade = CascadeType.MERGE)
+    private Category category;
+
+    @Column(name = "orderable_status")
+    private String orderableStatus;
+
+
+    public MenuAndCategory() {
+    }
+
+    public MenuAndCategory(int menuCode, String menuName, int menuPrice, Category category, String orderableStatus) {
+        this.menuCode = menuCode;
+        this.menuName = menuName;
+        this.menuPrice = menuPrice;
+        this.category = category;
+        this.orderableStatus = orderableStatus;
+    }
+
+    public int getMenuCode() {
+        return menuCode;
+    }
+
+    public void setMenuCode(int menuCode) {
+        this.menuCode = menuCode;
+    }
+
+    public String getMenuName() {
+        return menuName;
+    }
+
+    public void setMenuName(String menuName) {
+        this.menuName = menuName;
+    }
+
+    public int getMenuPrice() {
+        return menuPrice;
+    }
+
+    public void setMenuPrice(int menuPrice) {
+        this.menuPrice = menuPrice;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public String getOrderableStatus() {
+        return orderableStatus;
+    }
+
+    public void setOrderableStatus(String orderableStatus) {
+        this.orderableStatus = orderableStatus;
+    }
+
+    @Override
+    public String toString() {
+        return "MenuAndCategory{" +
+                "menuCode=" + menuCode +
+                ", menuName='" + menuName + '\'' +
+                ", menuPrice=" + menuPrice +
+                ", category=" + category +
+                ", orderableStatus='" + orderableStatus + '\'' +
+                '}';
+    }
+}
